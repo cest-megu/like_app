2acec19b5d541dfdae3c625f2b3813e3c32497ee
Megurin <tutimur_0521@yahoo.co.jp>
2021-03-29 23:23:28 +0900

    ゲストログインの実装

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+  has_many :posts, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  has_many :liked_posts, through: :likes, source: :post
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  def self.guest
+    find_or_create_by!(email: "guest@example.com") do |user|
+      user.password = SecureRandom.urlsafe_base64
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: {
+    registrations: "users/registrations",
+    passwords: "users/passwords"
+  }
+  root to: "posts#index"
+  resources :posts do
+    resource :likes, only: [:create, :destroy]
+  end
+
+  devise_scope :user do
+    post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
+  end
 end
